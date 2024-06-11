45e808b85d7c2d5f8f586ddef611bad71d19ffbc
mathieu blanchon <mathieu.b86@gmail.com>
2024-06-11 15:07:39 +0200

    dog controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,22 +1,37 @@
 class DogsController < ApplicationController
   def index
+    @dogs = Dog.all
   end
 
   def show
+    @dog = Dog.find(params[:id])
   end
 
   def new
+    @dog = Dog.new
   end
 
   def create
+    @dog = Dog.new(dog_params)
   end
 
   def edit
+    @dog = Dog.find(params[:id])
   end
 
   def update
+    @dog = Dog.find(params[:id])
+    @dog.update(dog_params)
   end
 
   def delete
+    @dog = Dog.find(params[:id])
+    @dog.destroy
   end
+
+private
+
+def dog_params
+  params.require(:dog).permit(:name, :description, :breed, :city, :start_date, :end_date)
+end
 end
