8e1cc856361cc300ff212c2d2ebadfe5d789578d
Luiza Nobrega <mmluiza_@hotmail.com>
2024-06-11 17:31:22 +0200

    validations faites

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,9 @@
 class Dog < ApplicationRecord
   belongs_to :user
+  has_many :user
+
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 2, maximum: 30 }
+  validates :city, presence: true
+  validates :description, presence: true
+  validates :breed, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :dogs
+  has_many :bookings
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 2, maximum: 30 }
+  validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 2, maximum: 30 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: "must be a valid email address" }
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: "must include at least one letter and one number" }
+  validates :city, presence: true
 end
